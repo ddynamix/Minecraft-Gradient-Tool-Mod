fa106d40278d1efad608bc35132d1d1e713601ad
Tyler Steptoe <tytysteptoe@gmail.com>
2026-09-09 15:15:11 -0400

    initial commit

---
+++ src/main/java/net/tyler/gradientwant/GradientWandDataGenerator.java
@@ -0,0 +1,11 @@
+package net.tyler.gradientwant;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GradientWandDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/tyler/gradientwant/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.tyler.gradientwant;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Client-side initialization code here
+    }
+}
